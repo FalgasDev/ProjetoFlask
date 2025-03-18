0954c835789a4d53e9283b180aa4dcd3c757cc5e
Fábio Garrote <fgarrote12@gmail.com>
2025-03-18 16:19:45 -0300

    feat: add the professor POST route

--- app.py
+++ app.py
@@ -1,6 +1,22 @@
-from flask import Flask
+from flask import Flask, jsonify, request
 
 app = Flask(__name__)
+professores = []
+
+# ---- Rota Post Professores ---- #
+@app.route('/professores', methods = ['POST'])
+def addProfessor():
+    data = request.json
+
+    professores.append({
+        "name": data['name'], 
+        "id": len(professores) + 1, 
+        "age": data['age'], 
+        "subject": data['subject'], 
+        "info": data['info']
+        })
+    
+    return jsonify({'success': True})
 
 if __name__ == '__main__':
     app.run(debug=True)
