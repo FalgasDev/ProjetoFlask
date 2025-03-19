1732917c822cd396e71fb58abe6aedd75ef16dea
FalgasDev <fgarrote12@gmail.com>
2025-03-19 09:33:09 -0300

    feat: test add students

--- tests.py
+++ tests.py
@@ -17,6 +17,43 @@ class TestStringMethods(unittest.TestCase):
 
         self.assertEqual(type(obj_retornado),type([]))
 
+    # ---- Testa se o POST da rota /alunos está adicionando os alunos ---- #
+    def test_001_adiciona_alunos(self):
+        requests.post('http://localhost:5000/alunos',json={
+            "name": "Fábio",
+            "age": 20, 
+            "class": 1, 
+            "bornDate": "18/09/2004", 
+            "firstGrade": 2, 
+            "secondGrade": 4,
+            "finalAverage": 3 
+            })
+        requests.post('http://localhost:5000/alunos',json={
+            "name": "Luiz",
+            "age": 18, 
+            "class": 1, 
+            "bornDate": "25/06/2006", 
+            "firstGrade": 5, 
+            "secondGrade": 7,
+            "finalAverage": 6 
+            })
+        
+        r_lista = requests.get('http://localhost:5000/alunos')
+        lista_retornada = r_lista.json()
+
+        achei_fabio = False
+        achei_luiz = False
+        for aluno in lista_retornada:
+            if aluno['name'] == 'Fábio':
+                achei_fabio = True
+            if aluno['name'] == 'Luiz':
+                achei_luiz = True
+        
+        if not achei_fabio:
+            self.fail('aluno fernando nao apareceu na lista de alunos')
+        if not achei_luiz:
+            self.fail('aluno roberto nao apareceu na lista de alunos')
+
 def runTests():
         suite = unittest.defaultTestLoader.loadTestsFromTestCase(TestStringMethods)
         unittest.TextTestRunner(verbosity=2,failfast=True).run(suite)
