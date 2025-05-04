be0d1faf3e52b6bd80c02cf86cc05edc3b23e896
Fábio Garrote <fgarrote12@gmail.com>
2025-05-04 18:32:02 -0300

    fix: fixing the student model

--- aluno/aluno_model.py
+++ aluno/aluno_model.py
@@ -1,83 +1,125 @@
 from errors import EmptyStringError, IdNotExist
+from datetime import date, datetime
 from config import db
+from turma.turma_model import Classroom
 
-class Professor(db.Model):
-    __tablename__ = "professors"
+class Student(db.Model):
+    __tablename__ = "students"
 
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     name = db.Column(db.String(100), nullable=False)
-    age = db.Column(db.Integer, nullable=False)
-    subject = db.Column(db.String(100), nullable=False)
-    info = db.Column(db.String(255), nullable=True)
+    class_id = db.Column(db.Integer, db.ForeignKey("classroom.id"))
+    born_date = db.Column(db.String)
+    first_grade = db.Column(db.Float)
+    second_grade = db.Column(db.Float)
+    final_average = db.Column(db.Float)
+    age = db.Column(db.Integer)
 
-    def __init__(self, name, age, subject, info):
+    def __init__(self, name, class_id, born_date, first_grade, second_grade):
         self.name = name
+        self.class_id = class_id
+        self.born_date = born_date
+        self.first_grade = first_grade
+        self.second_grade = second_grade
+        self.calculate_final_grade()
+        self.calculate_age()
+
+    def calculate_age(self):
+        born_date = self.born_date.split("-")
+        today = date.today()
+        age = today.year - int(born_date[0])
+        if (today.month, today.day) < (int(born_date[1]), int(born_date[2])):
+            age -= 1
         self.age = age
-        self.subject = subject
-        self.info = info
+
+    def calculate_final_grade(self):
+        final_average = (float(self.first_grade) + float(self.second_grade)) / 2
+        self.final_average = final_average
 
     def to_dict(self):
         return {
             "id": self.id,
             "name": self.name,
+            "class_id": self.class_id,
+            "born_date": self.born_date,
+            "first_grade": self.first_grade,
+            "second_grade": self.second_grade,
+            "final_average": self.final_average,
             "age": self.age,
-            "subject": self.subject,
-            "info": self.info
         }
 
-def addProfessor(data):
-    if 'name' and 'age' and 'subject' and 'info' not in data:
+def addStudent(data):
+    classroom = Classroom.query.get(data['class_id'])
+
+    if 'name' and 'class_id' and 'born_date' and 'first_grade' and 'second_grade' not in data:
         raise KeyError
     
-    if data['name'] == "" or data['age'] == "" or data['subject'] == "" or data['info'] == "":
+    if data['name'] == "" or data['class_id'] == "" or data['born_date'] == "" or data['first_grade'] == "" or data['second_grade'] == "":
         raise EmptyStringError
+    
+    if not classroom:
+        raise IdNotExist('O Id da classe não existe.')
+    
+    datetime.strptime(data['born_date'], '%Y-%m-%d')
 
-    professor = Professor(
+    student = Student(
         name=data['name'],
-        age=data['age'],
-        subject=data['subject'],
-        info=data['info']
+        class_id=data['class_id'],
+        born_date=data['born_date'],
+        first_grade=data['first_grade'],
+        second_grade=data['second_grade']
     )
 
-    db.session.add(professor)
+    db.session.add(student)
     db.session.commit()
 
-def getProfessors():
-    professors = Professor.query.all()
-    return [professor.to_dict() for professor in professors]
 
-def getProfessorById(id):
-    professor = Professor.query.get(id)
+def getStudents():
+    students = Student.query.all()
+    return [student.to_dict() for student in students]
 
-    if not professor:
-        raise IdNotExist("O professor não foi encontrado.")
+
+def getStudentById(id):
+    student = Student.query.get(id)
+
+    if not student:
+        raise IdNotExist("O aluno não foi encontrado.")
     
-    return professor.to_dict()
+    return student.to_dict()
 
-def updateProfessor(id, data):
-    professor = Professor.query.get(id)
 
-    if not professor:
-        raise IdNotExist("O professor que você quer atualizar não foi encontrado.")
+def updateStudent(id, data):
+    student = Student.query.get(id)
+    classroom = Classroom.query.get(data['class_id'])
 
-    if 'name' and 'age' and 'subject' and 'info' not in data:
+    if not student:
+        raise IdNotExist("O aluno que você quer atualizar não foi encontrado.")
+    
+    if 'name' and 'class_id' and 'born_date' and 'first_grade' and 'second_grade' not in data:
         raise KeyError
     
-    if data['name'] == "" or data['age'] == "" or data['subject'] == "" or data['info'] == "":
+    if data['name'] == "" or data['class_id'] == "" or data['born_date'] == "" or data['first_grade'] == "" or data['second_grade'] == "":
         raise EmptyStringError
-
-    professor.name = data['name']
-    professor.age = data['age']
-    professor.subject = data['subject']
-    professor.info = data['info']
-
+    
+    if not classroom:
+        raise IdNotExist('O Id da classe não existe.')
+    
+    datetime.strptime(data['born_date'], '%Y-%m-%d')
+    
+    student.name = data['name']
+    student.class_id = data['class_id']
+    student.born_date = data['born_date']
+    student.first_grade = data['first_grade']
+    student.second_grade = data['second_grade']
+    student.calculate_final_grade()
+    student.calculate_age()
     db.session.commit()
 
-def deleteProfessor(id):
-    professor = Professor.query.get(id)
+def deleteStudent(id):
+    student = Student.query.get(id)
 
-    if not professor:
-        raise IdNotExist("O professor que você quer deletar não foi encontrado.")
+    if not student:
+        raise IdNotExist("O aluno que você quer deletar não foi encontrado.")
     
-    db.session.delete(professor)
+    db.session.delete(student)
     db.session.commit()
