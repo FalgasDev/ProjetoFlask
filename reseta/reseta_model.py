32809cf7e3a8d9ef1b0a83133d61cfe3cb1ddf8c
Fábio Garrote <fgarrote12@gmail.com>
2025-05-04 18:56:32 -0300

    feat: add database to reset model

--- reseta/reseta_model.py
+++ reseta/reseta_model.py
@@ -1,11 +1,8 @@
-from aluno.aluno_model import alunos, alunos_deletados
-from professor.professor_model import professores, professores_deletados
-from turma.turma_model import turmas, turmas_deletadas
+from config import db
 
-def resetArrays():
-    alunos.clear()
-    professores.clear()
-    turmas.clear()
-    professores_deletados.clear()
-    alunos_deletados.clear()
-    turmas_deletadas.clear()
+def resetTables():
+    db.drop_all()
+    db.session.commit()
+
+    db.create_all()
+    db.session.commit()
