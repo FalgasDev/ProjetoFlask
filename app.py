c8b6203247346bb91af41816294e8975e4593a8c
Fábio Garrote <fgarrote12@gmail.com>
2025-05-04 17:40:18 -0300

    feat: add database config at app file

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from config import app
+from config import app, db
 from aluno.aluno_routes import aluno_blueprint
 from professor.professor_routes import professor_blueprint
 from turma.turma_routes import turma_blueprint
@@ -9,5 +9,12 @@ app.register_blueprint(professor_blueprint)
 app.register_blueprint(turma_blueprint)
 app.register_blueprint(reset_blueprint)
     
+with app.app_context():
+    db.create_all()
+
 if __name__ == '__main__':
-    app.run(host=app.config['HOST'], port=app.config['PORT'], debug=app.config['DEBUG'])
+    app.run(
+        host=app.config["HOST"],
+        port=app.config["PORT"],
+        debug=app.config["DEBUG"]
+    )
